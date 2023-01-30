1cb87acd0f1c33bfefedd26ccebb94ff04a0a051
ShevoFirst <play.job@yandex.ru>
2023-01-30 17:08:11 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+}
+}
